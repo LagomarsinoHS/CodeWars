e4a256bdd1501413d00113a31000b071351e7c66
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2021-08-11 16:55:38 -0400

    asd

--- main.js
+++ main.js
@@ -1944,11 +1944,6 @@ console.log(typist2("BeiJingDaXueDongMen"), 31)
 console.log('-----------------------------');
 
 //Remove duplicate words (7 kyu)
-const removeDuplicateWords = s => {
-    const sarr = s.split(" ")
-    console.log(sarr);
-    let a = [...new Set(s.split(" "))].join(" ")
-    console.log(a);
-}
+const removeDuplicateWords = s => [...new Set(s.split(" "))].join(" ")
 console.log(removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
 //alpha beta gamma delta

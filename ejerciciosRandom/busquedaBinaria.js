3d0aa0511b1793411da30e946cea1ab1ce2a076f
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2022-11-10 21:52:36 -0300

    busqueda binaria agregada

---
+++ ejerciciosRandom/busquedaBinaria.js
@@ -0,0 +1,69 @@
+
+/*
+* Binary Search is a faster one that native ones O(logN) vs O(n)
+* Normally used on trees or SORTED arrays
+*/
+
+// To evoid max call stack problem
+const trampoline = fn => (...args) => {
+    let result = fn(...args)
+    while (typeof result === 'function') {
+        result = result()
+    }
+    return result
+}
+
+
+// Generate an array from 0 to 1000 -> [0 ....., 1000]
+const longArray = Array.from({ length: 1001 }, (_, i) => i)
+
+
+const normalSearch = (array, valueToSearch) => {
+    let time = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        time++
+        if (array[i] === valueToSearch) break;
+    }
+    return `It took ${time} units of time to find the result`
+}
+
+// Normal BinarySearch
+const binarySearch = (array, valueToSearch, time = 0) => {
+    const middle = Math.floor(array.length / 2)
+    time++
+    if (array[middle] === valueToSearch) return `Se demoró ${time} unidades de tiempo en encontrar el resultado`
+    else if (array[middle] > valueToSearch) {
+        return binarySearch(array.slice(middle, valueToSearch, time))
+    } else {
+        return binarySearch(array.slice(0, middle), valueToSearch, time)
+    }
+}
+
+
+//BinarySearch to be used with trampoline
+const binarySearchNew = (array, valueToSearch, time = 0) => {
+    const middle = Math.floor(array.length / 2)
+
+    time++
+    if (array[middle] === valueToSearch) return `It took ${time} units of time to find the result`
+    else if (valueToSearch > array[middle]) {
+        return () => binarySearchNew(array.slice(middle), valueToSearch, time)
+    } else {
+        return () => binarySearchNew(array.slice(0, middle), valueToSearch, time)
+    }
+}
+
+const valueToSearch = 1000
+
+const resNormalSearch = normalSearch(longArray, valueToSearch)
+console.log("Normal Search ->", resNormalSearch);
+
+//const resBB = busquedaBinaria(arrayLargo, 100)
+//console.log("Busqueda Binaria ->", resBB);
+
+const resTrampolineBinarySearch = trampoline(binarySearchNew)(longArray, valueToSearch)
+console.log("Binary Search with trampoline ->", resTrampolineBinarySearch);
+
+
+

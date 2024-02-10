6e8eb9d098f24cdcb98395c856b3a5adf98ecde7
Humberto Lagomarsino G. <h.lagomarsino.g@gmail.com>
2024-02-10 20:24:30 -0300

    agregue un par mas

--- leetCode/arrays/main.js
+++ leetCode/arrays/main.js
@@ -24,8 +24,45 @@ const exercises = {
         }
         return nums.length;
     },
+    removeDuplicates(nums) {
+        let idx = 1
+        while (idx < nums.length) {
+            if (nums[idx] === nums[idx - 1]) {
+                nums.splice(idx, 1)
+            } else {
+                idx++
+            }
+        }
+        return nums.length
+    },
+    searchInsert(nums, target) {
+        const idxOf = nums.indexOf(target)
+        if (idxOf === -1) {
+            for (let i = 0; i < nums.length; i++) {
+                if (nums[nums.length - 1] < target) return nums.length;
+                if (nums[i] < target) continue;
+                else return i
+            };
+        }
+        else return idxOf
+    },
+    searchInsertBinary(nums, target) {
+        let left = 0
+        let right = nums.length - 1
 
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2)
+
+            if (nums[mid] === target) return mid
+            else if (nums[mid] > target) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
+    }
 }
 
 
-console.log(exercises.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
+console.log(exercises.searchInsertBinary([1, 3, 5, 6], 7))

6273ef4d73492c2acd06ac1e973be7160299b702
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2024-02-17 20:22:41 -0300

    add string folder

---
+++ leetCode/strings/main.js
@@ -0,0 +1,10 @@
+
+const exercises = {
+    defangIPaddr() {
+        //https://leetcode.com/problems/defanging-an-ip-address/
+        return address.replace(/\./g, '[.]')
+    },
+}
+
+
+console.log(exercises.defangIPaddr)
